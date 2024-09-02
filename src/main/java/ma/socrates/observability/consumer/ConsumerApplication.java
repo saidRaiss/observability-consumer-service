a9cc3eedf7e9f55cc45bc7d2041892d5564dc04d
SAID RAISS <raissaid90@gmail.com>
2024-09-02 21:43:40 +0200

    init project

---
+++ src/main/java/ma/socrates/observability/consumer/ConsumerApplication.java
@@ -0,0 +1,13 @@
+package ma.socrates.observability.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/socrates/observability/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.socrates.observability.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
